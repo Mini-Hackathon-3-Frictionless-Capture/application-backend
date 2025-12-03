8c98edd1e1805243eeb3826830d6595292dcf33e
JangasCodingplace <janisgoesser92@gmail.com>
2025-12-03 21:36:34 +0100

    feat: create auth token view

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/auth/token", obtain_auth_token),
 ]
