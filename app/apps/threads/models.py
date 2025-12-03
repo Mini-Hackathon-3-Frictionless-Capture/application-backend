a4b6140249eb006367e53a01baa8588f64281e5e
JangasCodingplace <janisgoesser92@gmail.com>
2025-12-03 21:21:55 +0100

    feat: add thread app

--- app/apps/threads/models.py
+++ app/apps/threads/models.py
@@ -1,1 +1,64 @@
-# Create your models here.
+from django.conf import settings
+from django.db import models
+
+
+class Thread(models.Model):
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+    )
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="threads",
+    )
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+
+class ThreadMessage(models.Model):
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.CASCADE,
+        related_name="messages",
+    )
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+    )
+    content = models.TextField(
+        blank=True,
+    )
+    is_bot_message = models.BooleanField(
+        default=False,
+    )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="thread_messages",
+        null=True,
+    )
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+
+class ThreadMessageImageAttachment(models.Model):
+    thread = models.ForeignKey(
+        ThreadMessage,
+        on_delete=models.CASCADE,
+        related_name="image_attachments",
+    )
+    image = models.ImageField(
+        upload_to="threads/images/",
+    )
+
+
+class ThreadMessageAudioAttachment(models.Model):
+    thread = models.ForeignKey(
+        ThreadMessage,
+        on_delete=models.CASCADE,
+        related_name="audio_attachments",
+    )
+    audio = models.FileField(
+        upload_to="threads/audio/",
+    )
--- app/config/settings.py
+++ app/config/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split()
 # Application definition
 
 INSTALLED_APPS = [
+    "apps.threads.apps.ThreadsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
