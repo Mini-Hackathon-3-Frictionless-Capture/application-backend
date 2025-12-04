ed85de1f01611898a1ee466235ca29c0ea86b9d5
JangasCodingplace <janisgoesser92@gmail.com>
2025-12-04 09:10:14 +0100

    feat: add retrieve endpoint for thread messages

--- app/apps/threads/api.py
+++ app/apps/threads/api.py
@@ -1,6 +1,6 @@
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, get_object_or_404
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from . import models, serializers
 
@@ -16,3 +16,23 @@ class ThreadListCreateAPIView(ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class ThreadMessageRetrieveAPIView(RetrieveAPIView):
+    queryset = models.ThreadMessage.objects
+    serializer_class = serializers.ThreadMessageSerializer
+    permission_classes = (IsAdminUser,)
+    authentication_classes = (TokenAuthentication,)
+
+    def get_object(self):
+        user_id = self.kwargs.get("user_id")
+        thread_id = self.kwargs.get("thread_id")
+        thread_message_id = self.kwargs.get("thread_message_id")
+
+        filter_ = {
+            "id": thread_message_id,
+            "thread__id": thread_id,
+            "thread__owner__id": user_id,
+        }
+
+        return get_object_or_404(models.ThreadMessage.objects, **filter_)
--- app/apps/threads/urls.py
+++ app/apps/threads/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
         api.ThreadListCreateAPIView.as_view(),
         name="threads__threads",
     ),
+    path(
+        "user/<int:user_id>/thread/<int:thread_id>/message/<int:thread_message_id>",
+        api.ThreadMessageRetrieveAPIView.as_view(),
+        name="threads__user__thread__message",
+    ),
 ]
